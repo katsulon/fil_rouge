4c8a0455faca8390c5c4bcdc0fe256449d8cc55a
Elisa Goloviatinski <elisa.goloviatinski@he-arc.ch>
2023-04-04 00:44:46 +0200

    SFML test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,43 @@
+#include <SFML/Graphics.hpp>
 #include <list>
 #include "Hero.h"
 #include "class/ClassImpl.h"
 
 using namespace He_ARC::rpg;
 
+void test();
+
 int main() {
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Goloviatinski Fil Rouge");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen()) {
+        sf::Event ev;
+        while (window.pollEvent(ev))
+        {
+            switch (ev.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if(ev.key.code == sf::Keyboard::Escape)
+                        window.close();
+                    if(ev.key.code == sf::Keyboard::P)
+                        test();
+                    break;
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    return 0;
+}
+
+void test() {
     Warrior *war1 = new Warrior;
     Rogue *rog1 = new Rogue;
     Wizard *wzd1 = new Wizard;
@@ -30,5 +63,4 @@ int main() {
     for(Hero *member : party) {
         delete member;
     }
-    return 0;
 }
